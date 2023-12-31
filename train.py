53701f4458800c38bb9383707a80e3fc6c4653b4
Seosamh Cahill <seo.cahill@gmail.com>
2023-12-31 15:21:31 +0100

    refactor(training-script): wrap training code into main function and update dataset path
    Refactored `train.py` to encapsulate the training pipeline within a `main()` function for better readability and modularity. Adjusted the dataset path in `BaseDatasetConfig` to point to a new dataset version "caint_mhaigh_eo-0.0.1". Additionally, added an `eval_split_size` parameter to the `GlowTTSConfig`. The scope of execution is now controlled by checking if `__name__ == '__main__'`, which allows the script to be imported without execution side effects.

--- train.py
+++ train.py
@@ -4,70 +4,74 @@
 
 import os
 
-# BaseDatasetConfig: defines name, formatter and path of the dataset.
-from TTS.tts.configs.shared_configs import BaseDatasetConfig
+def main():
+    # BaseDatasetConfig: defines name, formatter and path of the dataset.
+    from TTS.tts.configs.shared_configs import BaseDatasetConfig
 
-output_path = "tts_train_dir"
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
+    output_path = "tts_train_dir"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-# Download and extract LJSpeech dataset.
-# !wget -O $output_path/LJSpeech-1.1.tar.bz2 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
-# !tar -xf $output_path/LJSpeech-1.1.tar.bz2 -C $output_path
+    # Download and extract LJSpeech dataset.
+    # !wget -O $output_path/LJSpeech-1.1.tar.bz2 https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+    # !tar -xf $output_path/LJSpeech-1.1.tar.bz2 -C $output_path
 
-dataset_config = BaseDatasetConfig(
-    formatter="ljspeech", meta_file_train="metadata.csv", path=os.path.join(output_path, "LJSpeech-1.1/")
-)
+    dataset_config = BaseDatasetConfig(
+        formatter="ljspeech", meta_file_train="metadata.csv", path=os.path.join(output_path, "caint_mhaigh_eo-0.0.1/")
+    )
 
-# GlowTTSConfig: all model related values for training, validating and testing.
-from TTS.tts.configs.glow_tts_config import GlowTTSConfig
-config = GlowTTSConfig(
-    batch_size=32,
-    eval_batch_size=16,
-    num_loader_workers=4,
-    num_eval_loader_workers=4,
-    run_eval=True,
-    test_delay_epochs=-1,
-    epochs=100,
-    text_cleaner="phoneme_cleaners",
-    use_phonemes=True,
-    phoneme_language="ga",
-    phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
-    print_step=25,
-    print_eval=False,
-    mixed_precision=True,
-    output_path=output_path,
-    datasets=[dataset_config],
-    save_step=1000,
-)
+    # GlowTTSConfig: all model related values for training, validating and testing.
+    from TTS.tts.configs.glow_tts_config import GlowTTSConfig
+    config = GlowTTSConfig(
+        batch_size=32,
+        eval_batch_size=16,
+        num_loader_workers=4,
+        num_eval_loader_workers=4,
+        eval_split_size=0.030303030303030304,
+        run_eval=True,
+        test_delay_epochs=-1,
+        epochs=100,
+        text_cleaner="phoneme_cleaners",
+        use_phonemes=True,
+        phoneme_language="ga",
+        phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+        print_step=25,
+        print_eval=False,
+        mixed_precision=True,
+        output_path=output_path,
+        datasets=[dataset_config],
+        save_step=1000,
+    )
 
-from TTS.tts.utils.text.tokenizer import TTSTokenizer
-tokenizer, config = TTSTokenizer.init_from_config(config)
+    from TTS.tts.utils.text.tokenizer import TTSTokenizer
+    tokenizer, config = TTSTokenizer.init_from_config(config)
 
-from TTS.utils.audio import AudioProcessor
-ap = AudioProcessor.init_from_config(config)
-# Modify sample rate if for a custom audio dataset:
-# ap.sample_rate = 22050
+    from TTS.utils.audio import AudioProcessor
+    ap = AudioProcessor.init_from_config(config)
+    # Modify sample rate if for a custom audio dataset:
+    # ap.sample_rate = 22050
 
 
-from TTS.tts.datasets import load_tts_samples
-train_samples, eval_samples = load_tts_samples(
-    dataset_config,
-    eval_split=True,
-    eval_split_max_size=config.eval_split_max_size,
-    eval_split_size=config.eval_split_size,
-)
+    from TTS.tts.datasets import load_tts_samples
+    train_samples, eval_samples = load_tts_samples(
+        dataset_config,
+        eval_split=True,
+        eval_split_max_size=config.eval_split_max_size,
+        eval_split_size=config.eval_split_size,
+    )
 
-from TTS.tts.models.glow_tts import GlowTTS
-model = GlowTTS(config, ap, tokenizer, speaker_manager=None)
+    from TTS.tts.models.glow_tts import GlowTTS
+    model = GlowTTS(config, ap, tokenizer, speaker_manager=None)
 
-from trainer import Trainer, TrainerArgs
-trainer = Trainer(
-    TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
-)
+    from trainer import Trainer, TrainerArgs
+    trainer = Trainer(
+        TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
+    )
 
-trainer.fit()
+    trainer.fit()
 
+if __name__ == '__main__':
+    main()
 # !pip install tensorboard
 # !tensorboard --logdir=tts_train_dir
 # not necessary built in ot vsc
